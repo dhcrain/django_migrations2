bd2dd0773d7d92875a0e322062fb4d8541896d6f
dhcrain <dhcrain@gmail.com>
2016-06-06 16:38:13 -0400

    first commit

---
+++ SJ/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class SjBaseballStat(models.Model):
+    full_name = models.CharField(max_length=30)
+    position = models.CharField(max_length=6)
+    number = models.IntegerField()
+    avg = models.FloatField()
+    hr = models.IntegerField()
+    rbi = models.IntegerField()
+    runs = models.IntegerField()
+
+    def __str__(self):
+        return self.full_name
